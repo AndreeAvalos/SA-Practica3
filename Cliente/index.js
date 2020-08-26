f2d94b2fe466daf6fe4011a1f4fb18f8428325af
Andree Avalos <aavalosoto@gmail.com>
2020-08-26 12:47:30 -0600

    Terminado

---
+++ Cliente/index.js
@@ -0,0 +1,49 @@
+//Librerias a usar
+'use strict';
+const express = require('express');
+const app = express();
+const axios = require("axios")
+var body_parser = require('body-parser').json();
+//puerto y ruta
+const PORT = 3000;
+const HOST = 'localhost';
+
+app.get('/', function(req, res) {
+    res.send("Servidor Cliente")
+});
+//url para obtener una orden
+app.get('/getorder', function(req,res){
+    var num = Math.floor(Math.random() * (1000-1)+1)
+    var descripcion = "Se creo una orden con id:"+num
+    axios.post('http://localhost:3003/log',{'descripcion':descripcion})
+    axios.post('http://localhost:3001/postorder',{'id':num})
+    res.json({'id':num})    
+})
+
+app.get('/getstateorder',body_parser,function(req,res){
+    var order = req.body.id
+    var descripcion = "Se desea saber el estado de orden:"+order
+    axios.post('http://localhost:3003/log',{'descripcion':descripcion})
+    axios.get('http://localhost:3001/getstate/'+order)
+        .then(function(response){
+            res.send(response['data'])
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+        });
+});
+
+app.get('/getstatedelivery',body_parser,function(req,res){
+    var order = req.body.id
+    var descripcion = "Se desea saber el estado de envio de orden:"+order
+    axios.post('http://localhost:3003/log',{'descripcion':descripcion})
+    axios.get('http://localhost:3002/getorder/'+order)
+    .then(function(response){
+        res.send(response['data'])
+    });
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # SA-Practica3
 Practica 3 de Software Avanzado
+
+Descripcion:
+* Realizar una aplicación SOA para simular un servicio de crowdsourcing de comida a domicilio.
+* 3 microservicios (no hace falta que tengan bases de datos):
+* * * Cliente
+* * * * Solicitar pedido al restaurante
+* * * * Verificar estado del pedido al restaurante
+* * * * Verificar estado del pedido al repartidor
+* * * Restaurante
+* * * * Recibir pedido del cliente
+* * * * Informar estado del pedido al cliente
+* * * * Avisar al repartidor que ya está listo el pedido
+* * * Repartidor
+* * * * Recibir pedido del restaurante
+* * * * Informar estado del pedido al cliente
+* * * * Marcar como entregado
+
+## Requisitos
+* Node js
+
+## Librerias
+
+```
+npm i express
+npm i fs
+npm i body-parser
+npm i axios
+```
+
+## Ejecucion
+```
+node Cliente/index.js
+node Restaurante/index.js
+node Repartidor/index.js
+node log/index.js
+```
+
+## Colaborador
+
+* Carlos Andree Avalos Soto 201408580
